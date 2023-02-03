babbc5357781b3f9aebb6f0fce0da2605357b47c
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-02-02 22:05:22 -0700

    Push #25
    >Testing Last Modified

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,6 +1,8 @@
 const currentDate = new Date();
 let year = currentDate.getFullYear();
 let lastUpdate = currentDate.toLocaleString();
+let lastModified = currentDate(document.lastModified);
 
 document.querySelector('#copyright').textContent = year;
 document.querySelector('#time').textContent = lastUpdate;
+document.querySelector('#LastMod').textContent = lastModified;
