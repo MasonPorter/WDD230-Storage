f98fe56d5a4804461440349a69aab14e714bcb08
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-02-03 13:31:44 -0700

    Push #27
    >CSS Update to Footer and Header

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,7 +1,7 @@
 const currentDate = new Date();
 let year = currentDate.getFullYear();
-//let lastUpdate = currentDate.toLocaleString();
+let day = currentDate.toDateString();
 
 document.querySelector('#copyright').textContent = year;
 document.querySelector('#lastMod').textContent = document.lastModified;
-//document.querySelector('#time').textContent = lastUpdate;
+document.querySelector('#currentDate').textContent = day;
