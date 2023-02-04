bf753730392641908f31677e94a4e4ca9030b93f
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-02-03 23:58:37 -0700

    Push #28
    >Completed Header and Footer

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,7 +1,15 @@
 const currentDate = new Date();
 let year = currentDate.getFullYear();
-let day = currentDate.toDateString();
+let day = currentDate.toLocaleDateString('en-us', {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'});
 
 document.querySelector('#copyright').textContent = year;
 document.querySelector('#lastMod').textContent = document.lastModified;
 document.querySelector('#currentDate').textContent = day;
+
+function toggleMenu() {
+    document.querySelector('#mainNav').classList.toggle('open');
+    document.querySelector('#navButton').classList.toggle('open');
+}
+
+const x = document.querySelector('#navButton')
+x.onclick = toggleMenu;
