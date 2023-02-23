92f588de33b7904b27085f00f0222c1281fc441a
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-02-23 00:55:02 -0700

    Push #44
    >Completed Tablet Viewport

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,7 +1,8 @@
 const currentDate = new Date();
 let year = currentDate.getFullYear();
 let day = currentDate.toLocaleDateString('en-us', {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'});
-let bannerDay = currentDate.getDay();
+//let bannerDay = currentDate.getDay();
+let bannerDay = 1;
 
 if (bannerDay == 1) {
     document.querySelector('#meetbanner').style.display = "block";
