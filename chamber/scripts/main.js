bdc42ebb366b08648d7f9366dfcf433022a03a1e
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-02-23 11:30:23 -0700

    Push #45
    >Finished Nav for Desktop

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,8 +1,8 @@
 const currentDate = new Date();
 let year = currentDate.getFullYear();
 let day = currentDate.toLocaleDateString('en-us', {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'});
-//let bannerDay = currentDate.getDay();
-let bannerDay = 1;
+let bannerDay = currentDate.getDay();
+//let bannerDay = 1;
 
 if (bannerDay == 1) {
     document.querySelector('#meetbanner').style.display = "block";
@@ -15,6 +15,7 @@ if (bannerDay == 1) {
 document.querySelector('#copyright').textContent = year;
 document.querySelector('#lastMod').textContent = document.lastModified;
 document.querySelector('#currentDate').textContent = day;
+document.querySelector('#currentDatedesktopview').textContent = day;
 
 function toggleMenu() {
     document.querySelector('#mainNav').classList.toggle('open');
