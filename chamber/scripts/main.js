28511271b45205933b96035ced61b9a315731135
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-02-25 18:39:07 -0700

    Push #49
    >Completed Lazy Loading and localstorage requirements.

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -22,5 +22,19 @@ function toggleMenu() {
     document.querySelector('#navButton').classList.toggle('open');
 }
 
-const x = document.querySelector('#navButton')
-x.onclick = toggleMenu;
+const visitsDisplay = document.querySelector("#visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls")); // Using the Number() function ensures that if the storage item does not exist, it will be converted into a zero (0) which helps on the if block condition.
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
