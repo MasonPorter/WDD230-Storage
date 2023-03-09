d8833a8dea1bd5bd575fdca260573fc635574fcf
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2023-03-08 19:42:23 -0700

    Push #62
    >Completed W10 Activity

--- lesson10/main.js
+++ lesson10/main.js
@@ -1,9 +1,20 @@
 const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fairbanks%20Alaska?unitGroup=us&key=4Q8UKYJEHX7HAYHK3ULAKHY3Q&contentType=json";
 
-const getWeather = async () => {
+async function getWeather() {
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
-  };
+    numberCrunch(data);
+};
   
 getWeather();
+
+const numberCrunch = (data) => {
+  const apiTemp = data.currentConditions.temp;
+  document.querySelector('#t').textContent = Math.round(apiTemp);
+  let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+  document.querySelector('#ws').textContent = data.currentConditions.windspeed;
+  document.querySelector('#condition').textContent = data.currentConditions.conditions;
+  document.querySelector('#weather_icon').src = image;
+  document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
+}
